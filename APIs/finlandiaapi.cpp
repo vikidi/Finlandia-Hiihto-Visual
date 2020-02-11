38b809662e3e5fb41b216d73f2ec73dce3c9389b
Ilkka Kallioniemi <ilkka.kallioniemi@tuni.fi>
2020-02-11 21:13:52 +0200

    Started implementing workers for backend

--- APIs/finlandiaapi.cpp
+++ APIs/finlandiaapi.cpp
@@ -20,34 +20,7 @@ FinlandiaAPI::~FinlandiaAPI()
 
 std::map<QString, std::map<QString, std::vector<std::vector<std::string> > > > FinlandiaAPI::loadAllData()
 {
-    std::vector<std::string> matkat = {
-        "P50",
-        "V50",
-        "P100",
-        "P32",
-        "V20",
-        "V32",
-        "V20jun",
-        "P42",
-        "V42",
-        "P20",
-        "P30",
-        "P44",
-        "P60",
-        "P62",
-        "P25",
-        "P35",
-        "P45",
-        "P52",
-        "P53",
-        "P75",
-        "V30",
-        "V45",
-        "V53",
-        "V75"
-    };
 
-    // REAL DEAL
     m_ready = 0;
     m_runners = 0;
     m_currentProgress = 0;
@@ -65,22 +38,26 @@ std::map<QString, std::map<QString, std::vector<std::vector<std::string> > > > F
     }
 
     std::vector<std::thread> threads;
-    m_totalCalls = static_cast<int>(matkat.size()*(2020-1974));
-    threads.reserve(matkat.size()*(2020-1974));
+    m_totalCalls = static_cast<int>(TRIPS.size()*(2020-1974));
 
-    int j = 0;
-    for(int i = 1974; i < 2020; ++i) {
-        for(auto m : matkat) {
-            while (m_runners >= optimalAmountOfThreads)
-            {
-                // Constant polling could be changed to event-based system using std::condition_variable
-                std::this_thread::sleep_for(std::chrono::microseconds(1));
-            }
-            threads.push_back(std::thread(&FinlandiaAPI::loadInThread, this, QString::number(i), QString::fromStdString(m)));
-            m_runners++;
-            j++;
-        }
+    auto searchVector = std::make_shared<std::vector<FinlandiaAPI::Parameters>>();
+    searchVector->reserve(2020-1974);
+
+    threads.reserve(optimalAmountOfThreads);
+
+
+    for(int i = 1974; i < 1985; ++i) {
+        FinlandiaAPI::Parameters search;
+        search.year = QString::number(i);
+        searchVector->push_back(search);
     }
+
+    for(int i(0); i < optimalAmountOfThreads; i++)
+    {
+        m_runners++;
+        threads.push_back(std::thread(&FinlandiaAPI::loadInThread, this, searchVector));
+    }
+
 
     for (auto& th : threads) {
         th.join();
@@ -89,21 +66,37 @@ std::map<QString, std::map<QString, std::vector<std::vector<std::string> > > > F
     // Loading ready
     emit progressChanged(100);
 
-    // TESTING
-    //std::thread th(&FinlandiaAPI::loadInThread, this, QString("2017"), QString("P50"));
-    //th.join();
-
     return m_data;
 }
 
-void FinlandiaAPI::loadInThread(QString year, QString distance)
+void FinlandiaAPI::loadInThread(std::shared_ptr<std::vector<FinlandiaAPI::Parameters>> searchVector)
 {
+    qDebug() << "Thread started";
     FinlandiaCaller caller;
 
-    std::vector<std::vector<std::string> > data = caller.loadAllData(year, distance);
+    bool thisRunnerIsReady(false);
 
-    appendData(data, year, distance);
-    m_runners--;
+    while (m_runners > 0) {
+        std::vector<std::vector<std::string> > data = caller.loadData(searchVector);
+        if(Q_LIKELY(data.size() > 0))
+        {
+            if(Q_UNLIKELY(thisRunnerIsReady))
+            {
+                m_runners++;
+                thisRunnerIsReady = false;
+            }
+            // NOTE: Appending data does not work
+            //appendData(data, QString::fromStdString(data.at(0).at(0)),QString::fromStdString(data.at(0).at(1)));
+        } else
+        {
+            if(!thisRunnerIsReady)
+            {
+                m_runners--;
+                thisRunnerIsReady = true;
+            }
+        }
+    }
+    qDebug() << "Thread ended";
 }
 
 void FinlandiaAPI::appendData(std::vector<std::vector<std::string>> data, QString year, QString distance)
--- APIs/finlandiaapi.h
+++ APIs/finlandiaapi.h
@@ -2,12 +2,42 @@
 #define FINLANDIAAPI_H
 
 #include <QObject>
+#include <memory>
 #include <map>
 #include <unordered_map>
 #include <vector>
 #include <iostream>
 #include <mutex>
 #include <atomic>
+
+
+const std::vector<std::string> TRIPS = {
+    "P50",
+    "V50",
+    "P100",
+    "P32",
+    "V20",
+    "V32",
+    "V20jun",
+    "P42",
+    "V42",
+    "P20",
+    "P30",
+    "P44",
+    "P60",
+    "P62",
+    "P25",
+    "P35",
+    "P45",
+    "P52",
+    "P53",
+    "P75",
+    "V30",
+    "V45",
+    "V53",
+    "V75"
+};
+
 
 class FinlandiaAPI : public QObject
 {
@@ -17,7 +47,21 @@ public:
     FinlandiaAPI();
     ~FinlandiaAPI();
 
+    struct Parameters {
+        QString year = "";
+        QString trip = "kaikki";
+        QString gender = "kaikki";
+        QString age = "kaikki";
+        QString firstName = "";
+        QString lastName = "";
+        QString city = "";
+        QString nationality = "0";
+        QString team = "";
+    };
+
+
     std::map<QString, std::map<QString, std::vector<std::vector<std::string>>>> loadAllData();
+
 
 signals:
     void progressChanged(const int);
@@ -25,7 +69,7 @@ signals:
 private slots:
 
 private:
-    void loadInThread(QString, QString);
+    void loadInThread(std::shared_ptr<std::vector<FinlandiaAPI::Parameters>>);
     void appendData(std::vector<std::vector<std::string> >, QString, QString);
 
     std::mutex m_mtx;
