69f14baadff05c6b39ce1e156f4674f4cf22ae31
Ilkka Kallioniemi <ilkka.kallioniemi@tuni.fi>
2020-03-11 22:55:36 +0200

    Added functions for hashing locally stored names

--- crypter.cpp
+++ crypter.cpp
@@ -1,21 +1,94 @@
 #include "crypter.h"
-#include "interfacefilter.h"
-#include <QString>
+#include "logger.h"
+#include <QFile>
+#include <QByteArray>
+#include <QList>
+#include <QDirIterator>
+
 
 InternetExplorers::Crypter::Crypter()
 {
+    auto msg(QString("Constructor ready"));
+    auto msgSender(QString("Crypter"));
+    InternetExplorers::Logger::getInstance().log(msg, InternetExplorers::Logger::Severity::INFO, msgSender);
+}
 
+InternetExplorers::Crypter::~Crypter()
+{
+    auto msg(QString("Destructor called"));
+    auto msgSender(QString("Crypter"));
+    InternetExplorers::Logger::getInstance().log(msg, InternetExplorers::Logger::Severity::INFO, msgSender);
 }
 
 void InternetExplorers::Crypter::hashNames(std::vector<std::vector<std::string>>& data,
                                            QCryptographicHash::Algorithm algorithm)
 {
+    auto msg(QString("Started function hashNames()"));
+    auto msgSender(QString("Crypter"));
+    InternetExplorers::Logger::getInstance().log(msg, InternetExplorers::Logger::Severity::INFO, msgSender);
+
     for(auto& row : data)
     {
         QCryptographicHash hash(algorithm);
-        hash.addData(row[InternetExplorers::InterfaceFilter::ValueFilters::NAME].c_str(),
-                row[InternetExplorers::InterfaceFilter::ValueFilters::NAME].length());
-        row[InternetExplorers::InterfaceFilter::ValueFilters::NAME]
-                = QString(hash.result()).toStdString().substr(6);
+        hash.addData(row[InternetExplorers::Crypter::IndexInData::NAME].c_str(),
+                row[InternetExplorers::Crypter::IndexInData::NAME].length());
+        row[InternetExplorers::Crypter::IndexInData::NAME]
+                = QString(hash.result().chopped(6)).toStdString();
     }
+
+    msg = QString("Done function hashNames()");
+    InternetExplorers::Logger::getInstance().log(msg, InternetExplorers::Logger::Severity::INFO, msgSender);
+}
+
+void InternetExplorers::Crypter::hashLocalNames(QCryptographicHash::Algorithm algorithm)
+{
+    auto msg(QString("Started hashing local data"));
+    auto msgSender(QString("Crypter"));
+    InternetExplorers::Logger::getInstance().log(msg, InternetExplorers::Logger::Severity::INFO, msgSender);
+    QDirIterator iterator(DATA_ROOT_NAME, QStringList() << DATA_FILE_NAME, QDir::NoFilter, QDirIterator::Subdirectories);
+    while (iterator.hasNext()) {
+        iterator.next();
+        hashLocalNamesInFile(iterator.filePath(), algorithm);
+    }
+    msg = QString("Done hashing local data");
+    InternetExplorers::Logger::getInstance().log(msg, InternetExplorers::Logger::Severity::INFO, msgSender);
+}
+
+void InternetExplorers::Crypter::hashLocalNamesInFile(QString filename, QCryptographicHash::Algorithm algorithm)
+{
+    QFile file(filename);
+    if (!file.open(QIODevice::ReadOnly | QIODevice::Text))
+    {
+        auto msg(QString("No local data found"));
+        auto msgSender(QString("Crypter"));
+        InternetExplorers::Logger::getInstance().log(msg, InternetExplorers::Logger::Severity::CRITICAL, msgSender);
+        return;
+    }
+
+    QString result;
+
+    while (!file.atEnd()) {
+        QByteArray line = file.readLine();
+        QList<QByteArray> splitRow(line.split(';'));
+
+        QCryptographicHash hash(algorithm);
+        hash.addData(splitRow.at(InternetExplorers::Crypter::IndexInData::NAME));
+        splitRow[InternetExplorers::Crypter::IndexInData::NAME]
+                = hash.result().chopped(6);
+        result.append(splitRow.join(';'));
+    }
+
+    file.close();
+    if (!file.open(QIODevice::WriteOnly | QIODevice::Truncate))
+    {
+        auto msg(QString("Local file write failed"));
+        auto msgSender(QString("Crypter"));
+        InternetExplorers::Logger::getInstance().log(msg, InternetExplorers::Logger::Severity::CRITICAL, msgSender);
+        return;
+    }
+
+    file.write(result.toUtf8());
+
+    file.close();
+
 }
--- crypter.h
+++ crypter.h
@@ -4,6 +4,7 @@
 #include <vector>
 #include <string>
 #include <QCryptographicHash>
+#include <QString>
 
 namespace InternetExplorers {
 
@@ -11,6 +12,7 @@ class Crypter
 {
 public:
     Crypter();
+    ~Crypter();
     /*!
      * \brief hashNames converts skier names in given data to their short hash version
      * \param data what to hash
@@ -18,6 +20,33 @@ public:
      */
     void hashNames(std::vector<std::vector<std::string>>& data,
                    QCryptographicHash::Algorithm algorithm = QCryptographicHash::Algorithm::Md5);
+    /*!
+     * \brief hashLocalNames converts skier names in data in disk to their short hash version
+     * \param algorithm what hash algorithm to use
+     */
+    void hashLocalNames(QCryptographicHash::Algorithm algorithm = QCryptographicHash::Algorithm::Md5);
+
+private:
+    enum IndexInData {
+        YEAR = 0,
+        DISTANCE,
+        TIME,
+        PLACE,
+        PLACE_MEN,
+        PLACE_WOMEN,
+        SEX,
+        NAME,
+        CITY,
+        NATIONALITY,
+        BIRTH_YEAR,
+        TEAM
+    };
+    const QString DATA_ROOT_NAME = "FinlandiaData";
+    const QString DATA_FILE_NAME = "Data.txt";
+    const QString MD5_DATA_FILE_NAME = "MD5_metadata.txt";
+    const QString META_DATA_FILE_NAME = "Metadata.txt";
+
+    void hashLocalNamesInFile(QString filename, QCryptographicHash::Algorithm algorithm);
 };
 
 }
