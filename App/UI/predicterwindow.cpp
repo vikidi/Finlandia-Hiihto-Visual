35187268a50a1b3f6c062f18d757b4f3161ca677
Ville Saarinen <ville.saarinen9@gmail.com>
2020-04-08 16:34:53 +0300

    Predicter shows the website

--- App/UI/predicterwindow.cpp
+++ App/UI/predicterwindow.cpp
@@ -7,11 +7,13 @@ PredicterWindow::PredicterWindow(QWidget *parent) :
 {
     ui->setupUi(this);
 
-    //view = new QWebEngineView(this);
-    //view->load(QUrl("http://finlandia-hiihto-predicter.herokuapp.com/"));
+    view = new QWebEngineView(this);
+    view->setGeometry(10,10,1280,780);
+    view->load(QUrl("http://finlandia-hiihto-predicter.herokuapp.com/"));
 }
 
 PredicterWindow::~PredicterWindow()
 {
+    delete view;
     delete ui;
 }
